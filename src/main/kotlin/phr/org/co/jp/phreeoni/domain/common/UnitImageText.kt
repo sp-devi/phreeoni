61012f129479e829315075278176de969fa81e40
spdevi <spneuro01@gmail.com>
2021-09-25 21:31:14 +0900

    Add VO for unitImage and text

--- src/main/kotlin/phr/org/co/jp/phreeoni/domain/common/UnitImageText.kt
+++ src/main/kotlin/phr/org/co/jp/phreeoni/domain/common/UnitImageText.kt
@@ -1,6 +1,4 @@
 package phr.org.co.jp.phreeoni.domain.common;
 
-import java.io.File
-
-data class UnitImageText(val id: Int, val imagePath: File, val coordinate: Coordinate) {
+data class UnitImageText(val id: Int, val text: String, val coordinate: Coordinate) {
 }
