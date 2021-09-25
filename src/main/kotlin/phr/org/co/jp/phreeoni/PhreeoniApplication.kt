f03c59b0dfca8163b94adc730e3cf8eab9ebb6b1
spdevi <spneuro01@gmail.com>
2021-09-25 21:30:42 +0900

    Add prototype run command

--- src/main/kotlin/phr/org/co/jp/phreeoni/PhreeoniApplication.kt
+++ src/main/kotlin/phr/org/co/jp/phreeoni/PhreeoniApplication.kt
@@ -2,10 +2,16 @@ package phr.org.co.jp.phreeoni
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import phr.org.co.jp.phreeoni.domain.recognizer.OCRecognizer
+import java.io.File
 
 @SpringBootApplication
 class PhreeoniApplication
 
 fun main(args: Array<String>) {
-	runApplication<PhreeoniApplication>(*args)
+   //  runApplication<PhreeoniApplication>(*args)
+    println("Starting image ocr...")
+    var imageFile = File("/home/spdevi/Pictures/conan_ocr_sample_1.png")
+    var ocrController = OCRecognizer(imageFile)
+    ocrController.processImageFile()
 }
---
+++ src/main/kotlin/phr/org/co/jp/phreeoni/domain/common/UnitImageText.kt
@@ -0,0 +1,6 @@
+package phr.org.co.jp.phreeoni.domain.common;
+
+import java.io.File
+
+data class UnitImageText(val id: Int, val imagePath: File, val coordinate: Coordinate) {
+}
