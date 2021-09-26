69d73d1debd263e9da5a42c385b0ad593550bd9e
spdevi <spneuro01@gmail.com>
2021-09-26 23:11:41 +0900

    Add parse function for bounding box

--- src/main/kotlin/phr/org/co/jp/phreeoni/domain/recognizer/OCRecognizer.kt
+++ src/main/kotlin/phr/org/co/jp/phreeoni/domain/recognizer/OCRecognizer.kt
@@ -1,9 +1,10 @@
 package phr.org.co.jp.phreeoni.domain.recognizer
 
 import net.sourceforge.tess4j.*;
+import phr.org.co.jp.phreeoni.domain.common.Coordinate
 import java.io.File
 
-class OCRecognizer (private val imageFile: File) {
+class OCRecognizer(private val imageFile: File) {
 
     fun processImageFile() {
         try {
@@ -13,9 +14,13 @@ class OCRecognizer (private val imageFile: File) {
             tesseract.setHocr(true)
             val result = tesseract.doOCR(imageFile)
             println(result)
-        } catch(e : Exception) {
+        } catch (e: Exception) {
             println(e)
         }
+    }
+
+    fun parseBoundingBoxFromOCR(ocrResult: String): Coordinate {
+        return Coordinate(-1f, -1f)
     }
 
 }
