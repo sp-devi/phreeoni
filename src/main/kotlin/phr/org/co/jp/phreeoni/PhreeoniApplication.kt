df374bb14004fae4742894836519d220b80ffa19
spdevi <spneuro01@gmail.com>
2021-10-03 23:04:40 +0900

    Change picture location

--- src/main/kotlin/phr/org/co/jp/phreeoni/PhreeoniApplication.kt
+++ src/main/kotlin/phr/org/co/jp/phreeoni/PhreeoniApplication.kt
@@ -10,7 +10,7 @@ class PhreeoniApplication
 fun main(args: Array<String>) {
    //  runApplication<PhreeoniApplication>(*args)
     println("Starting image ocr...")
-    val imageFile = File("/home/spdevi/Pictures/Screenshot_20210910_115900.png")
+    val imageFile = File("/tmp/Screenshot_20210910_115900.png")
     val ocrController = OCRecognizer(imageFile)
     ocrController.processImageFileDemo()
 }
