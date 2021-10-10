b52b16d867b14e2f2e5ae7ae914f00c561a9c1ca
spdevi <spneuro01@gmail.com>
2021-10-10 21:16:24 +0900

    Add parser function

--- src/main/kotlin/phr/org/co/jp/phreeoni/domain/ocr/OCRecognizer.kt
+++ src/main/kotlin/phr/org/co/jp/phreeoni/domain/ocr/OCRecognizer.kt
@@ -1,5 +1,6 @@
 package phr.org.co.jp.phreeoni.domain.ocr
 
+import com.lowagie.text.xml.XmlParser
 import net.sourceforge.tess4j.*;
 import phr.org.co.jp.phreeoni.domain.common.Coordinate
 import java.io.File
@@ -24,6 +25,7 @@ class OCRecognizer(private val imageFile: File) {
     }
 
     fun parseBoundingBoxFromOCR(ocrResult: String): Coordinate {
+        val parser: XmlParser = XmlParser();
         return Coordinate(-1f, -1f)
     }
 
