26eb5e8c7baae2bca19ea76255484d667f35c47d
spdevi <spneuro01@gmail.com>
2021-11-07 17:42:31 +0900

    Fix format

--- src/main/kotlin/phr/org/co/jp/phreeoni/domain/ocr/Parser.kt
+++ src/main/kotlin/phr/org/co/jp/phreeoni/domain/ocr/Parser.kt
@@ -1,13 +1,27 @@
 package phr.org.co.jp.phreeoni.domain.ocr
 
 import org.xml.sax.*
+import org.xml.sax.helpers.DefaultHandler
 import javax.xml.parsers.SAXParser
 import javax.xml.parsers.SAXParserFactory
 
 class Parser {
 
     fun getElement() {
-        val spf = SAXParserFactory.newInstance()
+        val parserFactory = SAXParserFactory.newInstance()
+        val parser = parserFactory.newSAXParser()
+        val defaultHandler = object : DefaultHandler() {
+            var currentValue = ""
+            var currentElement = ""
+
+            override fun startElement(uri: String?, localName: String?, qName: String?, attributes: Attributes?) {
+                super.startElement(uri, localName, qName, attributes)
+            }
+        }
+    }
+
+    fun readResult() {
+
     }
 
 }
